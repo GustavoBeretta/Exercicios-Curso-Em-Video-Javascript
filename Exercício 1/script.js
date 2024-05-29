9b745a5002cf4fee6c780193ad2e1c17c8126251
Gustavo Beretta Gonçalves <gustavoberettag@gmail.com>
2024-05-29 14:23:09 -0300

    exercicio1_completo

--- Exercício 1/script.js
+++ Exercício 1/script.js
@@ -6,13 +6,13 @@ function carregar() {
     console.log()
     if (hora > 18 || (hora == 18 && minuto > 46 ) || hora < 6 || (hora == 6 && minuto < 40)) {
         var cor = '#1b2846'
-        var fonte = '/images/noite.jpg'
+        var fonte = 'images/noite.jpg'
     } else if (hora > 12 || (hora == 12 && minuto >= 30)) {
         var cor = '#FFB37B'
-        var fonte = '/images/tarde.jpg'
+        var fonte = 'images/tarde.jpg'
     } else {
         var cor = '#409cd5'
-        var fonte = '/images/manha.jpg'
+        var fonte = 'images/manha.jpg'
     }
     document.body.style.background = cor
     document.getElementsByTagName('img')[0].src = fonte
